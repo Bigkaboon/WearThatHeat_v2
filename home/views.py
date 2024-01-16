253a2b3242c48b0434afb85ea617da4f3513207c
Bigkaboon <>
2024-01-16 16:41:25 +0000

    Made room for more than one Category per project and display new arrivals at home page

--- home/views.py
+++ home/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from products.models import Product
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'home/index.html')
+
+    new_arrivals = Product.objects.filter(category__name='new_arrivals')
+
+    context = {
+        'new_arrivals': new_arrivals,
+    }
+
+    return render(request, 'home/index.html', context)
