c7425513d1df1a9d480791cf8a8ebfa693f7a4c7
Bigkaboon <>
2024-01-21 08:18:09 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51Oaw1pDCxU8OJ34rqUlt46sdBS556wztzgdTW65kVMdS5CXRsMQss5tJJ3zKIDObU7aZJNYAkNAY4QW2Ncfykg9V00A4xktYst',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
