efa6b1ab70ef21a4bdc2e99a5eef7e66059917c6
Bigkaboon <>
2024-01-22 10:36:36 +0000

    fixed webhooks and updating in order to switch to gitpod

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -2,12 +2,12 @@ from django.http import HttpResponse
 from django.core.mail import send_mail
 from django.template.loader import render_to_string
 from django.conf import settings
-
 from .models import Order, OrderLineItem
 from products.models import Product
 
 import json
 import time
+import stripe
 
 class StripeWH_Handler:
     """Handle Stripe webhooks"""
@@ -16,21 +16,20 @@ class StripeWH_Handler:
         self.request = request
 
     def _send_confirmation_email(self, order):
+        """Send the user a confirmation email"""
         cust_email = order.email
         subject = render_to_string(
-            'checkout/templates/checkout/confirmation_emails/confirmation_email_subject.txt',
-            {'order': order,}
-        )
+            'checkout/confirmation_emails/confirmation_email_subject.txt',
+            {'order': order})
         body = render_to_string(
-            'checkout/templates/checkout/confirmation_emails/confirmation_email_body.txt',
-            {'order': order, 'contact_email': setting.DEFAULT_FROM_EMAIL}
-        )
+            'checkout/confirmation_emails/confirmation_email_body.txt',
+            {'order': order, 'contact_email': settings.DEFAULT_FROM_EMAIL})
 
         send_mail(
             subject,
             body,
-            setting.DEFAULT_FROM_EMAIL,
-            [custom_email]
+            settings.DEFAULT_FROM_EMAIL,
+            [cust_email]
         )
 
     def handle_event(self, event):
@@ -50,9 +49,13 @@ class StripeWH_Handler:
         bag = intent.metadata.bag
         save_info = intent.metadata.save_info
 
-        billing_details = intent.charges.data[0].billing_details
+        stripe_charge = stripe.Charge.retrieve(
+            intent.latest_charge
+        )
+
+        billing_details = stripe_charge.billing_details
         shipping_details = intent.shipping
-        grand_total = round(intent.charges.data[0].amount / 100, 2)
+        grand_total = round(stripe_charge.amount / 100, 2)
 
         # Clean data in the shipping details
         for field, value in shipping_details.address.items():
