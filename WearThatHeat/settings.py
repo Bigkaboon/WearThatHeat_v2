722d2ee198e63ce1e0fdd72d8b6b267f0c729e2d
Dexter Törnqvist Saramies <juicebravo1@gmail.com>
2024-01-29 14:55:03 +0000

    update, set debug to true if development in os.environ

--- WearThatHeat/settings.py
+++ WearThatHeat/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['8000-bigkaboon-wearthatheatv-2lc4ekie8wh.ws-eu107.gitpod.io', 'wear-that-heat-c81d8db45882.herokuapp.com', 'localhost']
 
