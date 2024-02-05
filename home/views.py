5299696c2da06dea248fc193591665c75ecad740
Dexter Törnqvist Saramies <juicebravo1@gmail.com>
2024-02-05 11:49:44 +0000

    added style, pagination functionality on HomePage and fixed sorting (Category sorting left to fix)

--- home/views.py
+++ home/views.py
@@ -1,14 +1,24 @@
 from django.shortcuts import render
 from products.models import Product
+from django.core.paginator import Paginator
 
 # Create your views here.
 
 def index(request):
 
     new_arrivals = Product.objects.filter(category__name='new_arrivals')
+    new_arrivals_list = new_arrivals.all()
+    paginator = Paginator(new_arrivals_list, 4)
+
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
 
     context = {
         'new_arrivals': new_arrivals,
+        'page_obj': page_obj,
     }
 
     return render(request, 'home/index.html', context)
+
+
+
--- products/views.py
+++ products/views.py
@@ -30,11 +30,13 @@ def all_products(request):
                 products = products.annotate(lower_name=Lower('name'))
             if sortkey == 'category':
                 sortkey = 'category__name'
+                unique_products = set(list(products))
+                products = list(unique_products)
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
                     sortkey = f'-{sortkey}'
-            products = products.order_by(sortkey)
+                products = products.order_by(sortkey)
             
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
@@ -51,8 +53,10 @@ def all_products(request):
             products = products.filter(queries)
 
     current_sorting = f'{sort}_{direction}'
-    unique_products = set(list(products))
-    products = list(unique_products)
+
+    
+
+
     context = {
         'products': products,
         'search_term': query,
@@ -61,7 +65,6 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
-
 
 def product_detail(request, product_id):
     """ A view to show individual product details """
