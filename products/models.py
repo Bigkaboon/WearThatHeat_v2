977588e05d90572eceb676163d77c55e2b7127b3
Dexter Törnqvist Saramies <juicebravo1@gmail.com>
2024-04-04 14:28:59 +0000

    added custom 404

--- products/models.py
+++ products/models.py
@@ -26,11 +26,9 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 
-class Outfit(models.Model):
-    name = models.CharField(max_length=254)
-    description = models.TextField()
-    products = models.ManyToManyField('Product')
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+class Outfit(Product):
+    product_list = models.ManyToManyField('Product', related_name='product_list')
+    full_price = models.DecimalField(max_digits=6, decimal_places=2)
 
     def __str__(self):
         return self.name
