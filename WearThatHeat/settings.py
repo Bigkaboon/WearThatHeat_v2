9e9e9e1b256b82e642366aa1b7e6121df15263e4
Dexter Törnqvist Saramies <juicebravo1@gmail.com>
2024-04-08 12:47:16 +0000

    debug true

--- WearThatHeat/settings.py
+++ WearThatHeat/settings.py
@@ -28,8 +28,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
-
+# 'DEVELOPMENT' in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-bigkaboon-wearthatheatv-fbi8ozql1jp.ws-eu110.gitpod.io', 'wear-that-heat-c81d8db45882.herokuapp.com', 'localhost']
 
